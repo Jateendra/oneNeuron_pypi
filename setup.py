af7c9e5e4e62417270c635d3b4d0a2210340c5c1
mypython123 <mypython.pradhan@gmail.com>
2021-09-22 00:55:44 +0530

    update readme

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "Jateendra"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.1",
+    version="0.0.2",
     author=USER_NAME,
     author_email="mypython.pradhan@gmail.com",
     description="It is an implementation of Perceptron",
